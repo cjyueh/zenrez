ea532966e807bb1fedf89c6528083404547458d7
cjyueh <cjyueh@gmail.com>
2017-10-14 23:36:22 -0700

    class-page: add close button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,11 +17,13 @@ function showAllClasses(data) {
   var classCardTemplate = $('#class-card-template').html();
   var template = Handlebars.compile(classCardTemplate);
   list.append(template(data));
-  // on click, add hash for class card in url
+  // on click...
   list.find($('.class__card')).on('click', function(e) {
     e.preventDefault();
     var classIndex = $(this).data('index');
+    // add hash id for class card to url
     window.location.hash = classIndex;
+
     selectClass = $(data[classIndex - 1]);
     console.log(selectClass[0]);
     showClassPage(selectClass[0]);
@@ -31,7 +33,14 @@ function showAllClasses(data) {
 // show single class info
 function showClassPage(data) {
   var classPage = $('.class-page');
-  var classPageTemplate = $('#class-page-template').html();
-  var template = Handlebars.compile(classPageTemplate);
+  var classPageContent = $('#class-page-template').html();
+  var template = Handlebars.compile(classPageContent);
   classPage.append(template(data));
+  classPage.addClass('show-page');
+
+  // close class page
+  classPage.find($('.class-page__close')).on('click', function(e) {
+    console.log('close clicked!');
+    classPage.removeClass('show-page');
+  });
 }
